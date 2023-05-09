4b915c0242b330ac7e00cb770daf8f421cb8848f
Ken Mwangi <kenmwangi071@gmail.com>
2023-05-09 15:03:04 +0300

    sample blog data

---
+++ app/data.ts
@@ -0,0 +1,34 @@
+const sampleBlogs = [
+  {
+    id: "1",
+    time: "May 9, 2023",
+    tags: "next 13",
+    title: "Next 13 is cool",
+    content:
+      "Short answer is no. Long answer the goal with the component gallery is to provide layout and treatment ideas with just enough fidelity to be useful.If you need an idea for a pricing page layout, the idea is that you'd browse the component gallery to find one you like, then combine that with what you learn from the book to create a great-looking design yourself.There are a million ways you could style these components, and we don't want the component gallery to encourage you to make every project look the same.",
+    name: "Ken Mwangi",
+    role: "Full stack Engineer",
+  },
+  {
+    id: "2",
+    time: "May 9, 2023",
+    tags: "prisma",
+    title: "Try Nextauth and Prisma",
+    content:
+      "Short answer is no. Long answer the goal with the component gallery is to provide layout and treatment ideas with just enough fidelity to be useful.If you need an idea for a pricing page layout, the idea is that you'd browse the component gallery to find one you like, then combine that with what you learn from the book to create a great-looking design yourself.There are a million ways you could style these components, and we don't want the component gallery to encourage you to make every project look the same.",
+    name: "Ken Mwangi",
+    role: "Full stack Engineer",
+  },
+  {
+    id: "3",
+    time: "May 9, 2023",
+    tags: "tailwindcss",
+    title: "Tailwindcss for styling",
+    content:
+      "Short answer is no. Long answer the goal with the component gallery is to provide layout and treatment ideas with just enough fidelity to be useful.If you need an idea for a pricing page layout, the idea is that you'd browse the component gallery to find one you like, then combine that with what you learn from the book to create a great-looking design yourself.There are a million ways you could style these components, and we don't want the component gallery to encourage you to make every project look the same.",
+    name: "Ken Mwangi",
+    role: "Full stack Engineer",
+  },
+];
+
+export default sampleBlogs;
