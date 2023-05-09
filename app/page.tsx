660b46961b37a2b73d8b767326f2e172c2361daa
Ken Mwangi <kenmwangi071@gmail.com>
2023-05-09 11:11:08 +0300

    Container wrapper

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Container from "./components/Container";
 
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <Container>
+        <h1>Home</h1>
+      </Container>
     </div>
   );
 };
