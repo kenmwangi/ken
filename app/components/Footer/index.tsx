f651d0b9b7afc0513091269f99fd20aa8b812e92
Ken Mwangi <kenmwangi071@gmail.com>
2023-05-16 15:13:13 +0300

    Components: Footer and Navbar links

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -4,50 +4,63 @@ import React from "react";
 import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
 import Container from "../Container";
 
+const footerNavs = [
+  { id: "1", href: "/about", title: "About" },
+  { id: "2", href: "/blog", title: "Blog" },
+  { id: "3", href: "/thoughts", title: "Thoughts" },
+];
+
+const github = <FaGithub />;
+const twitter = <FaTwitter />;
+const linkedin = <FaLinkedin />;
+
+const socialLinks = [
+  { id: "1", href: "https://github.com/kenmwangi", icon: github },
+  { id: "2", href: "https://twitter.com/ken_cipher", icon: twitter },
+  { id: "3", href: "https://www.linkedin.com/in/kkmwangi/", icon: linkedin },
+];
+
 const Footer = () => {
   return (
     <section className="border-t-[1px] border-neutral-100 p-12">
       <Container>
-        <div className="lg:flex  items-center">
-          <div className="flex mb-6 lg:mb-0 lg:mr-18 items-center">
-            <Link
-              href="/"
-              className="inline-block text-neutral-500 mr-5 hover:text-slate-800 rounded-md p-1"
-            >
-              <FaGithub size={18} />
-            </Link>
-
-            <Link
-              href="/"
-              className="inline-block text-neutral-500 mr-5 hover:text-slate-800 rounded-md p-1"
-            >
-              <FaTwitter size={18} />
-            </Link>
-
-            <Link
-              href="/"
-              className="inline-block text-neutral-500 mr-5 hover:text-slate-800 rounded-md p-1"
-            >
-              <FaLinkedin size={18} />
-            </Link>
+        <div className="flex flex-col gap-y-6 lg:justify-between lg:gap-y-0 lg:flex-row mb-6 lg:mb-0 lg:mr-18 lg:items-center">
+          <div className="">
+            {footerNavs.map((footerNav) => {
+              const { id, href, title } = footerNav;
+              return (
+                <Link
+                  key={id}
+                  href={href}
+                  className="hover:bg-neutral-100 text-sm transition rounded-full px-3 py-1.5  hover:text-slate-600"
+                >
+                  {title}
+                </Link>
+              );
+            })}
           </div>
-          <div className="flex mb-6 ml-auto lg:mb-0 items-center">
-            <Link
-              href="/about"
-              className="inline-block mr-4 sm:mr-10 text-xs font-semibold text-gray-500"
-            >
-              About
-            </Link>
-            <Link
-              href="/thoughts"
-              className="inline-block mr-4 sm:mr-10 text-xs font-semibold text-gray-500"
-            >
-              Thoughts
-            </Link>
+
+          <div>
+            <span className="inline-block ml-auto text-xs text-gray-500">
+              &copy; Ken Mwangi, {new Date().getFullYear()}. All rights reserved
+            </span>
           </div>
-          <span className="inline-block ml-auto text-xs text-gray-500">
-            &copy; Ken Mwangi, {new Date().getFullYear()}. All rights reserved
-          </span>
+
+          <div className="">
+            {socialLinks.map((socialLink) => {
+              const { id, href, icon } = socialLink;
+              return (
+                <Link
+                  key={id}
+                  href={href}
+                  target="_blank"
+                  className="inline-block text-neutral-500 mr-5 hover:text-slate-800 rounded-md p-1"
+                >
+                  {icon}
+                </Link>
+              );
+            })}
+          </div>
         </div>
       </Container>
     </section>
--- app/components/Navbar/index.tsx
+++ app/components/Navbar/index.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   return (
     <div className="border-b-[1px] border-neutral-100">
       <Container>
-        <div className="top-0 left-0 py-6 w-full flex-row items-center justify-between  text-slate-900 md:flex">
+        <div className="top-0 left-0 py-6 w-full flex-col lg:flex-row gap-6 flex flex-wrap items-center justify-between  text-slate-900">
           <Logo />
           <UserMenu />
         </div>
